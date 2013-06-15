1cb528f521f128669ecd0438c332d6561e98a03b
Jose Robinson <jrobinsonc@gmail.com>
2013-06-14 20:11:29 -0400

    jslint pass.

--- jquery.jorganz.js
+++ jquery.jorganz.js
@@ -1,66 +1,67 @@
+/*global jQuery, window*/
+
 /**
  * jOrganz v0.1
  * Grid layout for html blocks.
  * https://github.com/jrobinsonc/jorganz
  * MIT License
- * by Jose Robinson
+ * by JoseRobinson.com
  */
 
-(function($) {
-    
+(function ($) {
+
     "use strict";
 
-    $.fn.jOrganz = function() {
+    $.fn.jOrganz = function () {
 
         var $container = $(this),
-        $items = $container.children('div'),
-        row_max_items = 0;
+            $items = $container.children('div'),
+            row_max_items = 0;
 
         $container.css('position', 'relative');
         $items.css('position', 'absolute');
 
-        var order_items = function() {
+        function order_items() {
             var pos_top = [],
-            row_num = 0,
-            items_width = $items.eq(0).width(),
-            items_margin = [parseInt($items.eq(0).css('margin-left'), 10) + parseInt($items.eq(0).css('margin-right'), 10), parseInt($items.eq(0).css('margin-top'), 10) + parseInt($items.eq(0).css('margin-bottom'), 10)],
-            last_row_max_items = row_max_items;
+                row_num = 0,
+                items_width = $items.eq(0).width(),
+                items_margin = [parseInt($items.eq(0).css('margin-left'), 10) + parseInt($items.eq(0).css('margin-right'), 10), parseInt($items.eq(0).css('margin-top'), 10) + parseInt($items.eq(0).css('margin-bottom'), 10)],
+                last_row_max_items = row_max_items;
 
             row_max_items = Math.floor($container.width() / items_width);
 
-            if (row_max_items === last_row_max_items)
-                return;
+            if (row_max_items === last_row_max_items) { return; }
 
-            $.each($items, function(){
+            $.each($items, function () {
 
                 var $item = $(this);
 
-                row_num++;
+                row_num += 1;
 
                 if (row_num > row_max_items) {
                     row_num = 1;
                 }
 
-                if (pos_top[row_num-1] === undefined) {
-                    pos_top[row_num-1] = 0;
+                if (pos_top[row_num - 1] === undefined) {
+                    pos_top[row_num - 1] = 0;
                 }
 
                 $item.css({
-                    top: pos_top[row_num-1],
-                    left: (items_width + items_margin[0]) * (row_num-1)
+                    top: pos_top[row_num - 1],
+                    left: (items_width + items_margin[0]) * (row_num - 1)
                 });
 
-                pos_top[row_num-1] += ($item.height() + items_margin[1]);
+                pos_top[row_num - 1] += ($item.height() + items_margin[1]);
 
-                if (pos_top[row_num-1] > $container.height()){
-                    $container.height(pos_top[row_num-1]);
+                if (pos_top[row_num - 1] > $container.height()) {
+                    $container.height(pos_top[row_num - 1]);
                 }
             });
-        };
+        }
 
         order_items();
 
-        $(window).resize(function(){
+        $(window).resize(function () {
             order_items();
         });
 
